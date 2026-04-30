8d5b65f9665bc163d023280963c587d54ce23a74
nicole849 <lucynguzo26@gmail.com>
2026-04-30 15:49:40 +0300

    update

--- Reglog/src/Reglog.jsx
+++ Reglog/src/Reglog.jsx
@@ -1,18 +1,37 @@
 import React from "react";
 import "./Reglog.css";
-
+import user_icon from "./assets/person.jpg";
+import email_icon from "./assets/mail.jpg";
+import password_icon from "./assets/pass.jpg";
 
 const Reglog = () => {
   return (
-    <div className="reglog">
-        <div className="reglog-container">
+    <div className="container">
+        <div className="header">
         <div className="text">Sign Up</div>
         <div className="underline"></div>
          </div>
-      <div className="inputs"></div>
+      <div className="inputs">
       <div className="input">
-        
+        <img src={user_icon} alt="" />
+        <input type='text' placeholder='Username'/>
       </div>
+
+      <div className="input">
+        <img src={email_icon} alt="" />
+        <input type='email' placeholder='email'/>
+      </div>
+
+      <div className="input">
+        <img src={password_icon} alt="" />
+        <input type='password' placeholder='password'/>
+      </div>
+    </div>
+      <div className="forgot-password">LostPassword? <span>Click Here!</span></div>
+        <div className="submit-container">
+            <div className="submit">Sign Up</div>
+            <div className="submit">Login </div>
+        </div>
     </div>
   );
 }   
