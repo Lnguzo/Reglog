002e1bbd27ef4034ac25a428c1de5fbae40077df
nicole849 <lucynguzo26@gmail.com>
2026-05-07 15:33:31 +0300

    .

---
+++ Reglog/src/Dashboard.jsx
@@ -0,0 +1,21 @@
+import React  from "react";
+import "./Reglog .css";
+
+
+const Dashboard = ({user,onlogout}) => {
+    <Dashboard user={user} onlogout={handleLogout}/>
+    return (
+         <div className="dashboard-container">
+           <h1 className="Dashboard-title">Dashboard</h1>
+           <h2 className="Welcome-dashboard">welcome, {user.name}</h2>
+           <p className="Email">Email: {user.eamil}</p>
+
+           <button classname="logout-btn" 
+               onClick="OnLogout()">
+              Logout
+             </button>
+          </div>
+    );
+
+};
+export default Dashboard;
